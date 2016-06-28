87ad5e6682027fae964f735bfbf11a1738731ea3
seiyria <kyle@seiyria.com>
2016-06-27 19:21:08 -0500

    basic settings page

--- src/components/play/play.routes.js
+++ src/components/play/play.routes.js
@@ -5,10 +5,12 @@ import { CreateComponent } from './create/create.component';
 import { OverviewComponent } from './overview/overview.component';
 import { MapComponent } from './map/map.component';
 import { ChatComponent } from './chat/chat.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const routes = [
   { path: 'create',   canActivate: [AuthGuard], component: CreateComponent },
   { path: 'overview', canActivate: [AuthGuard], component: OverviewComponent },
   { path: 'map',      canActivate: [AuthGuard], component: MapComponent },
-  { path: 'chat',     canActivate: [AuthGuard], component: ChatComponent }
+  { path: 'chat',     canActivate: [AuthGuard], component: ChatComponent },
+  { path: 'settings', canActivate: [AuthGuard], component: SettingsComponent }
 ];
---
+++ src/components/play/settings/settings.component.js
@@ -0,0 +1,56 @@
+
+import { SweetAlertService } from 'ng2-sweetalert2';
+
+import _ from 'lodash';
+
+import { Component } from '@angular/core';
+import template from './settings.html';
+import './settings.less';
+
+const thanks = [
+  { name: 'Darkblizer', reason: 'Art' },
+  { name: 'Sedgwick', reason: 'Donation, Maps' },
+  { name: 'Latin', reason: 'Donation' },
+  { name: 'Marcus', reason: 'Donation' }
+];
+
+@Component({
+  providers: [SweetAlertService],
+  template
+})
+export class SettingsComponent {
+
+  static get parameters() {
+    return [[SweetAlertService]];
+  }
+
+  constructor(swal) {
+    this.swal = swal;
+  }
+
+  thanksHtml() {
+    return `
+<p>If you're interested in helping design content for the game, let Seiyria know.</p>
+
+<p><strong>Contributions:</strong></p>
+${_.map(thanks, t => `<div>${t.name} - ${t.reason}</div>`).join('')}
+
+<p></p>
+<p><strong>Want to help?</strong></p>
+<p>All support goes directly to the game and servers! Thanks in advance!</p>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+  <input type="hidden" name="cmd" value="_s-xclick">
+  <input type="hidden" name="hosted_button_id" value="TF5HJJLVYWMQU">
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+  <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+`;
+  }
+
+  showThanks() {
+    this.swal.swal({
+      title: 'Special Thanks',
+      html: this.thanksHtml()
+    });
+  }
+}
