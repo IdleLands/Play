073dd1e84d0443b532fdf8a5da0ea77693f6d1e3
seiyria <kyle@seiyria.com>
2016-07-03 09:29:30 -0500

    basic equipment page

---
+++ src/components/play/equipment/equipment.component.js
@@ -0,0 +1,34 @@
+
+import _ from 'lodash';
+
+import { Component } from '@angular/core';
+import { PrimusWrapper } from '../../../services/primus';
+
+import template from './equipment.html';
+import './equipment.less';
+
+@Component({
+  template
+})
+export class EquipmentComponent {
+  static get parameters() {
+    return [[PrimusWrapper]];
+  }
+
+  constructor(primus) {
+    this.primus = primus;
+  }
+
+  setPlayerData(data) {
+    this.player = data;
+    this.equipmentKeys = _.sortBy(_.keys(data.equipment));
+  }
+
+  ngOnInit() {
+    this.playerSubscription = this.primus.contentUpdates.player.subscribe(data => this.setPlayerData(data));
+  }
+
+  ngOnDestroy() {
+    this.playerSubscription.unsubscribe();
+  }
+}
--- src/components/play/play.routes.js
+++ src/components/play/play.routes.js
@@ -6,11 +6,13 @@ import { OverviewComponent } from './overview/overview.component';
 import { MapComponent } from './map/map.component';
 import { ChatComponent } from './chat/chat.component';
 import { SettingsComponent } from './settings/settings.component';
+import { EquipmentComponent } from './equipment/equipment.component';
 
 export const routes = [
   { path: 'create',   canActivate: [AuthGuard], component: CreateComponent },
   { path: 'overview', canActivate: [AuthGuard], component: OverviewComponent },
   { path: 'map',      canActivate: [AuthGuard], component: MapComponent },
   { path: 'chat',     canActivate: [AuthGuard], component: ChatComponent },
+  { path: 'equipment',canActivate: [AuthGuard], component: EquipmentComponent },
   { path: 'settings', canActivate: [AuthGuard], component: SettingsComponent }
 ];
