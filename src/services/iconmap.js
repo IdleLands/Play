3c78973f8f3a41650ef90c1e1fcd5abc4876a42d
seiyria <kyle@seiyria.com>
2016-07-07 16:46:07 -0500

    changing icons to be a more specific build

--- src/services/iconmap.js
+++ src/services/iconmap.js
@@ -2,30 +2,39 @@
 import _ from 'lodash';
 
 const StatIcons = {
-  str: 'strong',
-  dex: 'crosshair-arrow',
-  int: 'book-cover-2',
-  agi: 'run',
-  con: 'heart-organ',
-  luk: 'clover-2',
-  hp: 'nested-hearts',
-  mp: 'drop',
-  gold: 'cash'
+  str: 'stat-str',
+  dex: 'stat-dex',
+  int: 'stat-int',
+  agi: 'stat-agi',
+  con: 'stat-con',
+  luk: 'stat-luk',
+  hp:  'stat-hp',
+  mp:  'stat-mp',
+  gold: 'stat-gold'
 };
 
 const AdventureLogIcons = {
-  Meta: 'abstract-010',
-  Explore: 'world',
-  Levelup: 'beams-aura',
-  Achievement: 'ribbon-medal',
-  Combat: 'crossed-swords',
-  Pet: 'dove',
-  Guild: 'backup',
-  Towncrier: '',
-  Item: 'battle-gear',
-  Gold: 'profit',
-  Profession: 'body-swapping',
-  Xp: 'gift-of-knowledge'
+  Meta: 'symbol-meta',
+  Explore: 'symbol-explore',
+  Levelup: 'symbol-levelup',
+  Achievement: 'symbol-acheivement',
+  Combat: 'symbol-combat',
+  Pet: 'symbol-pet',
+  Guild: 'symbol-guild',
+  Towncrier: 'symbol-towncrier',
+  Item: 'symbol-item',
+  Gold: 'symbol-gold',
+  Profession: 'symbol-profession',
+  Xp: 'symbol-xp'
 };
 
-export const AllIcons = _.extend(StatIcons, AdventureLogIcons);
+const AchievementIcons = {
+  Progress: 'symbol-progress',
+  Explore: 'symbol-explore',
+  Combat: 'symbol-combat',
+  Special: 'symbol-special',
+  Event: 'symbol-event'
+
+};
+
+export const AllIcons = _.extend(StatIcons, AdventureLogIcons, AchievementIcons);
