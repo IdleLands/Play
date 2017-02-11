5eb288d654a1671b48542e6c3724159c81ab28ee
seiyria <kyle@seiyria.com>
2017-02-11 10:41:55 -0600

    update donate button to hopefully include player name

--- src/components/play/settings/settings.component.js
+++ src/components/play/settings/settings.component.js
@@ -66,9 +66,10 @@ ${_.map(thanks, t => `<div>${t.name} - ${t.reason}</div>`).join('')}
 <p></p>
 <p><strong>Want to help?</strong></p>
 <p>All support goes directly to the game and servers! Thanks in advance!</p>
-<p>Please visit <a href="https://github.com/IdleLands/IdleLands/wiki/Contributor-and-Donator-Perks" target="_blank">this wiki page</a> to see the perks of donating! Additionally, please include your chosen sprite and message in your donation notes, it makes it easier and faster to get you in the Hall of Heroes!</p>
+<p>Please visit <a href="https://github.com/IdleLands/IdleLands/wiki/Contributor-and-Donator-Perks" target="_blank">this wiki page</a> to see the perks of donating! Additionally, <strong>please include your chosen sprite and message in your donation notes</strong>, it makes it easier and faster to get you in the Hall of Heroes!</p>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
   <input type="hidden" name="cmd" value="_s-xclick">
+  <input type="hidden" name="custom" value="${this._playerData.name}">
   <input type="hidden" name="hosted_button_id" value="TF5HJJLVYWMQU">
   <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
   <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
@@ -162,6 +163,7 @@ ${_.map(thanks, t => `<div>${t.name} - ${t.reason}</div>`).join('')}
     const player = this.primus._contentUpdates.player.getValue();
 
     this._playerData = {
+      name: player.name,
       gender: player.gender,
       title: player.title,
       isMod: player.isMod
