425d1add4c986012756649522090473978bf89f1
seiyria <kyle@seiyria.com>
2017-02-24 13:15:05 -0600

    closes #143

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as _ from 'lodash';
 
 import { NavController } from 'ionic-angular';
+import 'reddit.js';
 
 import { Auth } from '../../services';
 
@@ -11,13 +14,26 @@ import { Primus } from '../../services';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  public redditPosts = [];
+  public didRedditFail: boolean;
 
   constructor(
     public navCtrl: NavController,
     public auth: Auth,
     public primus: Primus
   ) {}
+
+  ngOnInit() {
+    (<any>window).reddit.search('flair:News', 'idle_lands').restrict_sr(true).t('all').limit(3).sort('new').fetch(res => {
+      if(!res || !res.data || !res.data.children) {
+        this.didRedditFail = true;
+        return;
+      }
+      this.redditPosts = _.map(res.data.children, 'data');
+    }, () => this.didRedditFail = true);
+  }
 
   login() {
     this.auth.login()
