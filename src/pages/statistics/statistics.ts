19cc7ad982e30a8db990c87d18c7f117ab392e25
seiyria <kyle@seiyria.com>
2017-03-06 13:27:27 -0600

    closes #146

--- src/pages/statistics/statistics.ts
+++ src/pages/statistics/statistics.ts
@@ -17,6 +17,8 @@ export class StatisticsPage extends PlayComponent implements OnInit, OnDestroy {
   statistics$: any;
   statistics: Statistics;
 
+  private ignoreRecurseKeys = ['BossKills', 'Maps', 'Regions'];
+
   constructor(
     public appState: AppState,
     public primus: Primus,
@@ -42,9 +44,19 @@ export class StatisticsPage extends PlayComponent implements OnInit, OnDestroy {
   }
 
   setStatistics(data: Statistics) {
-    const recurse = (obj) => {
+    const BossKills = _.get(data, 'Character.BossKills', {});
+    const Maps = _.get(data, 'Character.Maps', {});
+    const Regions = _.get(data, 'Character.Regions', {});
+
+    (<any>data).BossKills = BossKills;
+    (<any>data).Maps = Maps;
+    (<any>data).Regions = Regions;
+
+    const recurse = (obj, isRoot = false) => {
       return _.map(obj, (val, key) => {
         const baseObj: any = {};
+
+        if(!isRoot && _.includes(this.ignoreRecurseKeys, key)) return {};
 
         baseObj.name = key;
 
@@ -68,7 +80,10 @@ export class StatisticsPage extends PlayComponent implements OnInit, OnDestroy {
       return _.sortBy(data, 'name');
     };
 
-    this.statistics = _.reject(sortAll(recurse(data)), item => item.val);
+    this.statistics = _.reduce(_.reject(sortAll(recurse(data, true)), item => item.val), (prev, item) => {
+      prev[item.name] = item;
+      return prev;
+    }, {});
   }
 
 }
