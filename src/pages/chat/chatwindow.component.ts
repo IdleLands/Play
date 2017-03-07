9ccf98d3d887fd07ecd5b96380cadad0f1c731a6
seiyria <kyle@seiyria.com>
2017-03-07 16:21:32 -0600

    attempt to increase chat component performance

--- src/pages/chat/chatwindow.component.ts
+++ src/pages/chat/chatwindow.component.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash';
 
-import { Component, EventEmitter, Input, Output, OnInit, OnChanges, OnDestroy, Pipe, PipeTransform, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnChanges, OnDestroy, Pipe, PipeTransform, ViewChild, ChangeDetectionStrategy } from '@angular/core';
 // import { NavController, MenuController } from 'ionic-angular';
 
 import { LocalStorage, LocalStorageService } from 'ng2-webstorage';
@@ -14,7 +14,8 @@ const AUTOSCROLL_THRESHOLD = 200;
 
 @Component({
   selector: 'chat-window',
-  templateUrl: 'chatwindow.html'
+  templateUrl: 'chatwindow.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChatWindowComponent implements OnInit, OnChanges, OnDestroy {
 
